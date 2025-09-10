d53a6fb7cf6c6027e685980b13854b148ee1f1fb
jmsharshil <harshil@jmsadvisory.in>
2025-09-10 18:14:10 +0530

    chnaged the requiremnt.txt

--- astrology_ai/settings.py
+++ astrology_ai/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#qw8)eodbrwq1lynha*w!p%v(i-@qxggz5@!jb-%ea1e7_r$7d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.206.172.164']
 
 
 # Application definition
